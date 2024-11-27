722488a2576303dbcfa9712d79bb602c3a4607ac
alexey <alexey>
2024-11-27 18:17:28 +0300

    add contact class

--- personal_assistant.py
+++ personal_assistant.py
@@ -6,6 +6,7 @@ import logging
 import pandas as pd
 import operator
 
+
 class Note:
     def __init__(self, title, content, timestamp=None, id=None):
         if id is None:
@@ -34,7 +35,8 @@ class Note:
     @staticmethod
     def from_json(note_json):
         return Note(**note_json)
-      
+
+
 class Task:
     def __init__(self, title, description, priority, due_date, id=None, done=None):
         if id is None:
@@ -77,3 +79,31 @@ class Task:
     @staticmethod
     def from_json(task_json):
         return Task(**task_json)
+
+
+class Contact:
+    def __init__(self, name, phone, email, id=None):
+        if id is None:
+            id = str(uuid.uuid4())
+        self.id = id
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+    def edit_contact(self, new_name=None, new_phone=None, new_email=None):
+        if new_name is not None:
+            self.name = new_name
+        if new_phone is not None:
+            self.phone = new_phone
+        if new_email is not None:
+            self.email = new_email
+
+    def to_json(self):
+        return dict(id=self.id, name=self.name, phone=self.phone, email=self.email)
+
+    def __str__(self):
+        return f"Контакт: {self.name} ({self.phone}, {self.email})"
+
+    @staticmethod
+    def from_json(contact_json):
+        return Contact(**contact_json)
